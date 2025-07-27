126fb3e5f385b081a529eaa838bac66344fd3b21
Mystic <Mystic@Yugs-MacBook-Pro.local>
2025-07-27 16:00:35 +0530

    Deployment bug fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/Clinkd/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
